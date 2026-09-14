c379bc63e41e0d83170527eee07dbc952f6a68c7
Sergey Potapchuk <vezhlivec@gmail.com>
2026-09-15 00:09:58 +0300

    Add HW 01.2 Networking

--- code/compute.tf
+++ code/compute.tf
@@ -1,42 +1,8 @@
-locals {
-  # Специализированный образ Yandex Cloud с уже настроенным NAT и ip_forward
-  image_id = "fd80mrhj8fl2oe87o4e1" 
-}
-
 data "yandex_compute_image" "ubuntu" {
   family = "ubuntu-2004-lts"
 }
 
-# 1. NAT-инстанс в публичной подсети
-resource "yandex_compute_instance" "nat" {
-  name        = "nat-instance"
-  platform_id = "standard-v3"
-  zone        = var.default_zone
-
-  resources {
-    cores  = 2
-    memory = 1
-    core_fraction = 20
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = local.image_id
-    }
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-
-  network_interface {
-    subnet_id  = yandex_vpc_subnet.public.id
-    ip_address = "192.168.10.254"
-    nat        = true
-  }
-}
-
-# 2. Публичная виртуальная машина (для проверки)
+# 1. Публичная виртуальная машина (для проверки)
 resource "yandex_compute_instance" "public-vm" {
   name        = "public-vm"
   hostname    = "public-vm"
@@ -69,7 +35,7 @@ resource "yandex_compute_instance" "public-vm" {
   }
 }
 
-# 3. Приватная виртуальная машина
+# 2. Приватная виртуальная машина
 resource "yandex_compute_instance" "private-vm" {
   name        = "private-vm"
   hostname    = "private-vm"
--- code/network.tf
+++ code/network.tf
@@ -18,12 +18,17 @@ resource "yandex_vpc_subnet" "private" {
   route_table_id = yandex_vpc_route_table.private-rt.id
 }
 
+resource "yandex_vpc_gateway" "my-gateway" {
+  name      = "my-gateway"
+  shared_egress_gateway {}
+}
+
 resource "yandex_vpc_route_table" "private-rt" {
   name       = "private-rt"
   network_id = yandex_vpc_network.my-network.id
 
   static_route {
     destination_prefix = "0.0.0.0/0"
-    next_hop_address   = "192.168.10.254"
+    gateway_id         = yandex_vpc_gateway.my-gateway.id
   }
 }
